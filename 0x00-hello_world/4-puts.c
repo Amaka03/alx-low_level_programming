35ef89faa43230e6e304dba246060dbc51c7b9f5
Amaka03 <ndukweamaka03@gmail.com>
2023-06-15 15:58:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return:0 (Success)
  */
 int main(void)
 {
-	("\"Programming is like building a multilingual puzzle");
+puts	("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 	
