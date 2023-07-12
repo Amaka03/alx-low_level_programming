7a1aa2f35493920685e42b4a52fc0c1dc07a8a5b
Amaka03 <ndukweamaka03@gmail.com>
2023-07-11 21:20:43 -0700

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,26 +7,28 @@
  *
  * Return: number of words
  */
+
 int count_word(char *s)
 {
-	int x, y, z;
+	int flag, c, w;
 
-	x = 0;
-	z = 0;
+	flag = 0;
+	w = 0;
 
-	for (y = 0; s[y] != '\0'; y++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[y] == ' ')
-			x = 0;
-		else if (x == 0)
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			x = 1;
-			z++;
+			flag = 1;
+			w++;
 		}
 	}
 
-	return (z);
+	return (w);
 }
+
 /**
  * **strtow - splits a string into words
  * @str: string to split
@@ -34,13 +36,14 @@ int count_word(char *s)
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
+
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, j = 0, k = 0, words, c = 0, start, end;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + k))
-		k++;
+	while (*(str + len))
+		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
@@ -49,7 +52,7 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i <= k; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
@@ -62,8 +65,8 @@ char **strtow(char *str)
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-				matrix[j] = tmp - c;
-				j++;
+				matrix[k] = tmp - c;
+				k++;
 				c = 0;
 			}
 		}
@@ -71,7 +74,7 @@ char **strtow(char *str)
 			start = i;
 	}
 
-	matrix[j] = NULL;
+	matrix[k] = NULL;
 
 	return (matrix);
 }
